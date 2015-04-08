36b3a0f405811e5217fa6f745230152508d91048
rodrigodiaz <rodrigo_diaz@jabil.com>
2015-04-08 15:38:05 -0500

    New structure

---
+++ main/main.go
@@ -0,0 +1,14 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+    var i = 5
+    fmt.Println("Hello, 世界")
+    fmt.Print("Hi")
+    fmt.Print("Yeah",i)
+}
+
+    
